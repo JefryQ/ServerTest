571acf36834d212f2dcf1c667e370503636558db
Jefry Quejada <jefryqt18@gmail.com>
2024-04-12 19:34:52 -0500

    modificar server.js v2

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
-import { createServer } from 'http';
+/* const http = require('http');
 
 const hostname = 'localhost';
-const port = 3000;
+const port = 3000; */
 
 /* const server = http.createServer((req, res) => {
   res.statusCode = 200;
@@ -10,13 +10,30 @@ const port = 3000;
 }); */
 
 
-function requestController(){
+/* function requestController(){
   console.log("¡Hola desde mi servidor Node.js!\n")
 }
 
-const server = createServer(requestController)
+const server = http.createServer(requestController)
 
 server.listen(port, hostname, () => {
   console.log(`Servidor iniciado en http://${hostname}:${port}`);
 });
+ */
 
+import http from 'http';
+
+const hostname = 'localhost';
+const port = 3000;
+
+function requestController(req, res) {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('¡Hola desde mi servidor Node.js!\n');
+}
+
+const server = http.createServer(requestController);
+
+server.listen(port, hostname, () => {
+  console.log(`Servidor iniciado en http://${hostname}:${port}`);
+});
